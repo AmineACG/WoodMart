8798362e9113fd1716898202f361ee97e447962a
elmehdi <mehdiblabo@gmail.com>
2023-09-08 09:22:25 +0100

    initialisation de project

---
+++ src/test/java/com/example/demo/ECommerceTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
